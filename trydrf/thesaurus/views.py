df8f879e0373cc7852c94be94022ac91f5f86765
sentenzo <kassiyskley@rambler.ru>
2022-08-04 20:59:58 +0300

    APIView as a base class

--- trydrf/thesaurus/views.py
+++ trydrf/thesaurus/views.py
@@ -1,10 +1,34 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from .models import Word
+from .models import Language, Word
 from .serializers import WordSerializer
 
 
-class WordsApiView(generics.ListAPIView):
-    queryset = Word.objects.all()
-    serializer_class = WordSerializer
+class WordsApiView(APIView):
+    def get(self, request):
+        words = Word.objects.all().values()
+        return Response({"words": words})
+
+    def post(lf, request):
+        langs = {"en": "English", "ru": "Русский"}
+        # fmt: off
+        new_word = Word.objects.create(
+            spelling= request.data['spelling'],  # "apple", 
+            ipa_transcription = request.data['ipa_transcription'],
+            definition = request.data['definition'],
+            language = Language.objects.get(
+                name=langs[request.data["language_code"]]
+            ),
+        )
+        # fmt: on
+
+        return Response({"word": model_to_dict(new_word)})
+
+
+# class WordsApiView(generics.ListAPIView):
+#     queryset = Word.objects.all()
+#     serializer_class = WordSerializer
